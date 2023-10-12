b4315ce1e63a74b041b518c368476eb62c1b67d3
Yashs Tiwari <yashstiwari03@gmail.com>
2023-10-12 12:43:53 +0530

    corrected typos

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,13 @@
-import express from express;
-import {getFeedPosts, getUserPosts, likePost} from "../controllers/posts.js";
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 import router from "./auth.js";
 
 /* READ */
-router.get("/",verifyToken,getFeedPosts);
-router.get("/:userId/posts",verifyToken,getUserPosts);
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
-router.patch("/:id/like",verifyToken,likePost);
+router.patch("/:id/like", verifyToken, likePost);
 
 export default router;
